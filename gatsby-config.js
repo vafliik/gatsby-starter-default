ae586698e4a70a18c7234bca8a4415e368c835c6
Pavel Pribyl <pribyl.pavel@gmail.com>
2019-12-03 23:52:26 +0100

    Sample Bootstrap template

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,21 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Source Sans Pro\:200,200i,300,300i,400,400i,600,600i,700,700i,900,900i`,
+          `Merriweather\:300,300i,400,400i,700,700i,900,900i`
+        ]
+      }
+    },
+    {
+      resolve: 'gatsby-plugin-mailchimp',
+      options: {
+        endpoint: 'https://gmail.us4.list-manage.com/subscribe/post?u=5ae5ac19e8a9971f2ac42f3a3&amp;id=fca62eef9f',
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,17 +4,55 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import NewsLetter from "../components/newsletter"
+
+import { FaTwitter,FaFacebookF,FaInstagram, FaFacebook } from 'react-icons/fa'
+
+const bgVideo = require("../dist/mp4/bg.mp4")
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div className="overlay"></div>
+    <video playsinline="playsinline" autoplay="autoplay" muted="muted" loop="loop">
+      <source src={bgVideo} type="video/mp4" />
+    </video>
+
+    <div className="masthead">
+      <div className="masthead-bg"></div>
+      <div className="container h-100">
+        <div className="row h-100">
+          <div className="col-12 my-auto">
+            <div className="masthead-content text-white py-5 py-md-0">
+              <h1 className="mb-3">Coming Soon!</h1>
+              <p className="mb-5">We're working hard to finish the development of this site. Our target launch date is
+              <strong>January 2019</strong>! Sign up for updates using the form below!</p>
+              <NewsLetter />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+
+    <div className="social-icons">
+      <ul className="list-unstyled text-center mb-0">
+        <li className="list-unstyled-item">
+          <a href="#">
+            <FaTwitter />
+          </a>
+        </li>
+        <li className="list-unstyled-item">
+          <a href="#">
+            <FaFacebook />
+          </a>
+        </li>
+        <li className="list-unstyled-item">
+          <a href="#">
+            <FaInstagram />
+          </a>
+        </li>
+      </ul>
+    </div>
   </Layout>
 )
 
